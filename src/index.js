dd0ee677153046b62bf65b68f8c6a4e156e38854
w4d3 <morph@r2d2.r2d2.com>
2025-07-22 21:26:53 -0500

    corrections

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: true,
     credentials: true,
   })
 );
@@ -29,14 +29,6 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 server.listen(PORT, () => {
   console.log("server corriendo en el puerto:" + PORT);
